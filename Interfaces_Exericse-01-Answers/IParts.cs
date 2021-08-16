0201da80a0a3238b64330b82bd1ac76333031f7a
johndward01 <johndward01@gmail.com>
2021-08-16 16:49:38 -0500

    Add project files.

---
+++ Interfaces_Exericse-01-Answers/IParts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces_Exericse_01_Answers
+{
+    // TODO: Create an Interface called IParts
+    public interface IParts
+    {
+        // TODO: Create a BuildPart() stubbed out method that has a return type of IParts
+        IParts BuildPart();
+    }
+}
+ 
---
+++ Interfaces_Exericse-01-Answers/Program.cs
@@ -0,0 +1,68 @@
+﻿using Interfaces_Exericse_01_Answers.Vehicle_Parts;
+using System;
+using System.Collections.Generic;
+
+namespace Interfaces_Exericse_01_Answers
+{
+    class Program
+    {
+        // TODO: Create an Interface called IParts
+        // TODO: Create a BuildPart() stubbed out method that has a return type of IParts       
+
+        // TODO: Create a class called PlanePart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called TruckPart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called CarPart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called BikePart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called MiscPart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+
+        // BONUS
+        // TODO: Create methods to abstract away most of the code from your Main() method
+        // TODO: Create a Utility class to encapsulate those methods into 1 location
+
+        static void Main(string[] args)
+        {
+            // TODO: Create an IParts List called partsList that will hold all of the parts we want to create
+            var partsList = new List<IParts>();
+
+            // TODO: Create a do-while loop that only ends when the user types "no" (or 'n')
+            Utility.Greet();
+            do
+            {
+                // TODO: Greet the user and ask them which part they would like to create
+                var part = Utility.GetPart();
+
+                // TODO: Call that parts BuildPart() method
+                part.BuildPart();
+
+                // TODO: Add the part to the partsList
+                partsList.Add(part);
+
+                // TODO: Ask them if they would like to continue or exit the program
+                var continuing = Utility.Continue();
+                if (continuing == "n" || continuing == "no")
+                {
+                    break;
+                }
+            } while (true);
+
+            // TODO: Print out all the parts in the parts list to the console
+            Console.WriteLine("PARTS LIST:");
+            int counter = 0;
+            foreach (var part in partsList)
+            {
+                Console.WriteLine($"Part #{counter}: {part.GetType().Name}");
+                counter++;
+            }
+        }
+    }
+}
